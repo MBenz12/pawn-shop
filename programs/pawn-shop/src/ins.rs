6684549ae986d22e4353cdbf5768454ac9e0a3dd
mbenz12 <mbenz10120112@gmail.com>
2022-12-23 21:53:06 -0500

    added drain function and fixed smart contract

--- programs/pawn-shop/src/ins.rs
+++ programs/pawn-shop/src/ins.rs
@@ -47,6 +47,26 @@ pub struct Fund<'info>
 }
 
 #[derive(Accounts)]
+pub struct Drain<'info>
+{
+    #[account(mut, address = pawn_shop.authority)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            PAWN_SHOP_SEED.as_bytes(),
+            pawn_shop.name.as_bytes(),
+        ],
+        bump = pawn_shop.bump,
+    )]
+    pub pawn_shop: Account<'info, PawnShop>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
 pub struct UpdatePawnShop<'info>
 {
     #[account(mut, address = pawn_shop.authority)]
