e9deb1309b47e016842c076126fb4949064500d8
mbenz12 <mbenz10120112@gmail.com>
2022-12-28 02:36:50 -0500

    added admin wallet

--- app/pawn-shop-dapp/config.ts
+++ app/pawn-shop-dapp/config.ts
@@ -3,5 +3,6 @@ export const HYPERSPACE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIi
 export const PAWNSHOP_NAME = "pawn-shop-defisociety";
 export const authorizedAdmins = [
     "3qWq2ehELrVJrTg2JKKERm67cN6vYjm1EyhCEzfQ6jMd",
+    "AWi4gdw3WW3jSZtK37N1GnGeyXPtCCh4tWQhueAoXW6V",
 ]
 export const JWT_TOKEN = "pawn-shop";
